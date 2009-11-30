9af01f9264e59b8abbac892ddb50eca90ad233c6
unknown <Chris@.(none)>
2009-11-30 09:06:12 -0800

    Updated registrar have SimpleRestful working now

--- app/Graphite.Web.Controllers/RouteRegistrar.cs
+++ app/Graphite.Web.Controllers/RouteRegistrar.cs
@@ -1,5 +1,8 @@
 ﻿#region
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using System.Web.Routing;
 using MvcContrib.SimplyRestful;
@@ -12,24 +15,20 @@ namespace Graphite.Web.Controllers {
     public static void RegisterRoutesTo(RouteCollection routes) {
       routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
       routes.IgnoreRoute("{*favicon}", new {favicon = @"(.*/)?favicon.ico(/.*)?"});
-      // The areas below must be registered from greater subareas to fewer;
-      // i.e., the root area should be the last area registered
-      // Example illustrative routes with a nested area - note that the order of registration is important
-      //routes.CreateArea("Organization/Department", "Graphite.Web.Controllers.Organization.Department",
-      //    routes.MapRoute(null, "Organization/Department/{controller}/{action}", new { action = "Index" }),
-      //    routes.MapRoute(null, "Organization/Department/{controller}/{action}/{id}")
-      //);
-      //routes.CreateArea("Organization", "Graphite.Web.Controllers.Organization",
-      //    routes.MapRoute(null, "Organization/{controller}/{action}", new { action = "Index" }),
-      //    routes.MapRoute(null, "Organization/{controller}/{action}/{id}")
-      //);
-      routes.CreateArea("Admin", "Graphite.Web.Controllers.Admin",
-        routes.MapRoute(null, "Admin/{controller}/{id}/{action}", new {action = "Show"}),
-        routes.MapRoute(null, "Admin/{controller}/{action}", new {controller = "Home", action = "Index"}));
-      // Routing config for the root area
-      routes.CreateArea("Root", "Graphite.Web.Controllers",
-        routes.MapRoute(null, "{controller}/{id}/{action}", new {action = "Show"}),
-        routes.MapRoute(null, "{controller}/{action}", new {controller = "Home", action = "Index"}));
+      routes.CreateArea("Admin", "Graphite.Web.Controllers.Admin",GetRestfulRoutes("Admin/"));
+      routes.CreateArea("Root", "Graphite.Web.Controllers",GetRestfulRoutes(""));
+    }
+
+    private static Route[] GetRestfulRoutes(string areaPrefix) {
+      var routes = new RouteCollection();
+      SimplyRestfulRouteHandler.BuildRoutes(routes, areaPrefix + "{controller}", null, null);
+      routes.Add(GetDefaultRoute(areaPrefix));
+      return routes.Cast<Route>().ToArray();
+    }
+
+    private static Route GetDefaultRoute(string areaPrefix) {
+      return new Route(areaPrefix + "{controller}/{action}",
+        new RouteValueDictionary(new {controller = "Home", action = "Index"}), new MvcRouteHandler());
     }
   }
 }
