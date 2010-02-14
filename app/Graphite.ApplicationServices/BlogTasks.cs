c78626f35e557869fa9e17bf50c592e96bcc3697
chnicola <chnicola@gmail.com>
2010-02-13 16:34:35 -0800

    Added blog class and some settings

---
+++ app/Graphite.ApplicationServices/BlogTasks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Graphite.Core;
+
+namespace Graphite.ApplicationServices
+{
+	public class BlogTasks
+	{
+	}
+
+	public interface IBlogTasks{
+		Blog GetBlog(Guid id);
+
+		IEnumerable<Blog> GetAllBlogs();
+      
+		IEnumerable<Blog> GetBlogsForUser(string username);
+	}
+}
---
+++ app/Graphite.Core/Blog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using SharpArch.Core.DomainModel;
+
+namespace Graphite.Core
+{
+	public class Blog : EntityWithTypedId<Guid>{
+		public string Name { get; set; }
+		public string Url { get; set; }
+	}
+}
